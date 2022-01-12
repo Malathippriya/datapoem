25046b441343f35356495603b65178797dde0c44
Malathippriya <93131214+Malathippriya@users.noreply.github.com>
2022-01-12 13:11:41 +0530

    Delete projectRepository.java

--- projectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assessment2.datapoem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.assessment2.datapoem.project;
-
-@Repository
-public interface projectRepository extends JpaRepository<project, Long>{
-
-}
